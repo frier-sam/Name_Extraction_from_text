d26e6cdd14adbd6cac545c181cebf4f53a85985d
sam <shop2local@gmail.com>
2018-09-02 03:16:14 +0530

    updated with some changes
    changed from list to dict for performance improvement and names with 3 and 4 words can be identified

--- name-extraction.py
+++ name-extraction.py
@@ -9,33 +9,64 @@ from nltk.corpus import wordnet as wn
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
+import re
+replacingwords = ['.', '-']
+Punclist = ['’','“','”','would','could','since']
 
+#this fuction finds all probable names(duplicates included) and numbers in a given text and returns
+#2 dictionaries names and numbers respectively
 
 def names(text):
-    twords = word_tokenize(text)
-    stop_words = stopwords.words('english') +  list(string.punctuation)
-    names = []
+    compiles = re.compile('|'.join(map(re.escape, replacingwords)))
+    uptex = compiles.sub(" ", text)
+    twords = word_tokenize(uptex)
+    stop_words = stopwords.words('english') +  list(string.punctuation) + Punclist
+    names = {}
+    nums = {}
     for n,word in enumerate(twords):
-        if word not in stop_words:
-            if len(wn.synsets(word)) == 0 : #or check if name in common list of words
-                if len(names)==0: 
-                    names.append([n,word])
-                elif names[-1][0] == (n - 1) :
-                    names[-1][1] = str(names[-1][1]) + " " + str(word)
-                else :
-                    names.append([n,word])
-    return names
+        if word.lower() not in stop_words:
+            if word.isnumeric():
+                nums[n] = word
+            if len(wn.synsets(word)) == 0 and not word.isnumeric(): #or check if name in common list of words
+                names[n]=word
+    return names,nums
+
+
+#this fuction takes the names dictionary from names fuction and joins these
+#names based on the position of then in text
+
+#this fuction returns a set of all joined names
+
+
+def fullnames(na):
+    lis = list(na.keys())
+
+    fullnames = {}
+    for k in lis:
+        fullnames[k] = na[k]
+        if k+1 in lis:
+            fullnames[k] = fullnames[k] + " " + na[k+1]
+            lis.remove(k+1)
+        if k+2 in lis:
+            fullnames[k] = fullnames[k] + na[k+2]
+            lis.remove(k+2)
+            
+    #uncomment below code if names have more than 3 words
+    #     if k+3 in lis:
+    #         fullnames[k] = fullnames[k] + na[k+3]
+    #         lis.remove(k+3)
     
+    return set(fullnames.values())    
+
+
     
-#example    
-#text = "hi umasai,mohit, eric,himanshu kumar and vaibav are doing some work . please #get back to divya"    
-#print(names(text))
+#example :-    
+#text = "hi umasai,mohit, eric,himanshu kumar and vaibav are doing some work . please get back to divya"    
+##usage
+#namedict,numberdict = names(text) #gives 2 dictionaries with names and numers
+#joinednames  = fullnames(namedict)
+#print(joinednames)
 #
 #
 #
-#result - [[1, 'umasai'],
-# [3, 'mohit'],
-# [5, 'eric'],
-# [7, 'himanshu kumar'],
-# [10, 'vaibav'],
-# [20, 'divya']]
+#result - {'umasai','mohit','eric','himanshu kumar', 'vaibav','divya'}
